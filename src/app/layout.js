f4548b3235dfb3a55d9fb82097f44bd89c69c0cc
Kyle <reimers95@gmail.com>
2023-07-11 18:24:36 -0500

    DarkMode toggle added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import Header from '../components/Header'
+import Providers from './Providers'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -7,18 +8,24 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'IMDB Clone',
   description: 'This is the IMDB clone website',
+  icons:{
+    icon:'/favicon-16x16.png',
+  }
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Providers>
         {/* Header */}
         <Header/>
         {/* Navbar */}
 
         {/* SearchBox */}
-        {children}</body>
+        {children}
+        </Providers>
+        </body>
     </html>
   )
 }
